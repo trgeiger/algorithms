4dc6e2534ed3b1c179c3a33379ea5870bb5b6b00
Tayler Geiger <taylergeiger@gmail.com>
2017-05-12 15:03:07 -0500

    Create 2d-array-ds.py

---
+++ 2d-array-ds.py
@@ -0,0 +1,90 @@
+"""
+Context 
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task 
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Note: If you have already solved the Java domain's Java 2D Array challenge, you may wish to skip this challenge.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers describing 2D Array ; every value in  will be in the inclusive range of  to .
+
+Constraints
+
+Output Format
+
+Print the largest (maximum) hourglass sum found in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+"""
+
+
+""" SOLUTION
+"""
+
+def getHourglass(arr, i, j):
+    return(arr[i][j]+arr[i][j+1]+arr[i][j+2]+
+           arr[i+1][j+1]+
+           arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+
+def getLargestHG(arr):
+    maxran = len(arr)-2
+    # set initial value as first hourglass
+    maxHG = getHourglass(arr, 0, 0)
+    for i in range(maxran):
+        for j in range(maxran):
+            result = getHourglass(arr, i, j)
+            if result > maxHG:
+                maxHG = result
+    return maxHG
+
+print(getLargestHG(arr))
